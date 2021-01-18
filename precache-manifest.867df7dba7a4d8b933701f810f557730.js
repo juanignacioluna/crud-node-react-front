64f54c7abc0446f87cef24cc8c3f2416a5ae3a06
Juan Ignacio Luna <chinoluna11@gmail.com>
2021-01-18 02:41:50 -0300

    Updates

---
+++ precache-manifest.867df7dba7a4d8b933701f810f557730.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7233ae1d06cffc3618ca1e07639fcc72",
+    "url": "/crud-node-react-front/index.html"
+  },
+  {
+    "revision": "dfac136a9d79dbda476b",
+    "url": "/crud-node-react-front/static/css/main.52b17be9.chunk.css"
+  },
+  {
+    "revision": "bf48e2eafb8c55ac5529",
+    "url": "/crud-node-react-front/static/js/2.ac4fc72d.chunk.js"
+  },
+  {
+    "revision": "dfac136a9d79dbda476b",
+    "url": "/crud-node-react-front/static/js/main.6773e14f.chunk.js"
+  },
+  {
+    "revision": "4bd316894643d9b1c57a",
+    "url": "/crud-node-react-front/static/js/runtime-main.2390ae94.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/crud-node-react-front/static/media/logo.25bf045c.svg"
+  }
+]);
